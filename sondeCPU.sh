65972f1725bceeb0a8434ef36a26747df2ead7d2
ngkawani <ngkawani@gmail.com>
2026-03-06 14:41:29 +0100

    Final final final

--- sondeCPU.sh
+++ sondeCPU.sh
@@ -6,8 +6,8 @@ $DATE =$(date '+%Y-%m-%d-%H:%M:%S')
 #Fonctions
 
 get_cpu_usage(){
-    LocaL usage
-    usage=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
+    LocaL $usage
+    $usage=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
 
     if [[ -z "$usage" || "$usage" == *"e+"*]]; then
         echo "0"
--- sondeDISK.sh
+++ sondeDISK.sh
@@ -2,7 +2,7 @@
 $DATE =$(date '+%Y-%m-%d-%H:%M:%S')
 
 get_disk_usage(){
-    usage=$(df / | awk 'NR==2' {print $5} | sed 's/%//')
+    $usage=$(df / | awk 'NR==2' {print $5} | sed 's/%//')
 
     echo "Disque actuellement utilisé : $usage%"
 }
