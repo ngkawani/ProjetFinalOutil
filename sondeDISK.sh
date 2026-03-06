ccea1138fb71a4c6c52576c66de559ad76d4b2b7
ngkawani <ngkawani@gmail.com>
2026-03-06 15:48:36 +0100

    stp

--- sondeDISK.sh
+++ sondeDISK.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 get_disk_usage(){
-    usage=$(df / | awk 'NR==2' {print $5} | sed 's/%//')
-
-    echo "Disque actuellement utilisé : $usage%"
+    date=$(date '+%Y-%m-%d %H:%M:%S')
+    total=$(df -h / | awk 'NR==2 {print $5}')
+    echo "[$date] [DISQUE] Utilisation Globale: $total"
 }
 
 get_disk_usage
