15828614dc5f253f92db01965564eede4f40ae20
ngkawani <ngkawani@gmail.com>
2026-03-13 14:52:57 +0100

    Sans le \ c'est mieux ^^

--- storage.sh
+++ storage.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-rrdtool create /home/kawani/ProjetFinalOutil/system.rrd \
---step 60 \
-DS:cpu:GAUGE:120:0:100 \
-DS:ram:GAUGE:120:0:100 \
-DS:memory:GAUGE:120:0:100 \
-RRA:AVERAGE:0.1:1:1440 #sauvegarde 24h de la base
+rrdtool create /home/kawani/ProjetFinalOutil/system.rrd --step 60 DS:cpu:GAUGE:120:0:100 DS:ram:GAUGE:120:0:100 DS:memory:GAUGE:120:0:100 RRA:AVERAGE:0.1:1:1440
