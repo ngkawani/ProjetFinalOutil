2838e37ad5bb5852682a0528cb2bdc7c01601222
ngkawani <ngkawani@gmail.com>
2026-03-13 14:43:57 +0100

    Avec la piece jointe c mieux ^^

--- sondeDISK.sh
+++ sondeDISK.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 get_disk_usage(){
-    total=$(df -h / | awk 'NR==2 {print $5} | tr -d "%"')
+    total=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
     echo "[DISQUE] Utilisation Globale: $total"
 }
 
