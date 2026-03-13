5b693b86b0fd7b81f7e57a9e18df778d60dba706
ngkawani <ngkawani@gmail.com>
2026-03-13 17:01:51 +0100

    Update export_json.sh

--- export_json.sh
+++ export_json.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rrd=""
-export_file=""
+rrd="/home/kawani/ProjetFinalOutil/system.rrd"
+export_file="/home/kawani/ProjetFinalOutil/exportfile.json"
 
 /usr/bin/rrdtool xport --json --start -60m --step 60 \
     DEF:c=$rrd:cpu:AVERAGE \
     DEF:r=$rrd:ram:AVERAGE \
-    DEF:d=$rrd:disque:AVERAGE \
-    XPORT:c:"CPU" XPORT:r:"RAM" XPORT:d:"DISQUE" > $export_file
+    DEF:d=$rrd:memory:AVERAGE \
+    XPORT:c:"CPU" XPORT:r:"RAM" XPORT:d:"MEMORY" > $export_file
